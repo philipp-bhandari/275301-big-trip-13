7ca5986d9ce43186fb04ce5dde6870b25ef11aea
SokolovAG <sokolov.ag01@gmail.com>
2020-11-17 23:48:13 +0300

    Разделение проекта на модули и их отрисовка

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
-import {createTripInfo} from "./view/trip-info";
+import {createTripInfo} from "./view/trip-info.js";
+import {createTripSort} from "./view/trip-sort.js";
+import {createTripControls} from "./view/trip-controls.js";
+import {createTripFilter} from "./view/trip-filter.js";
+import {createTripList} from "./view/trip-list.js";
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteHeaderElement = document.querySelector('.page-header__container');
-const tripMainElement = siteHeaderElement.querySelector('.trip-main');
+const siteBodyElement = document.querySelector(`.page-body`);
+const siteHeaderElement = siteBodyElement.querySelector(`.page-header__container`);
+const tripMainElement = siteHeaderElement.querySelector(`.trip-main`);
+const siteMainElement = siteBodyElement.querySelector(`.page-main`);
+const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
+const tripMenuElement = tripMainElement.querySelector(`.trip-controls`);
 
-render(tripMainElement, createTripInfo(), 'beforeend');
+render(tripMainElement, createTripInfo(), `afterbegin`);
+render(tripMenuElement, createTripControls(), `beforeend`);
+render(tripMenuElement, createTripFilter(), `beforeend`);
+render(tripEventsElement, createTripSort(), `afterbegin`);
+render(tripEventsElement, createTripList(), `beforeend`);
 
