caaa3a9fcad922753dc99186328c4af0ac1edb19
SokolovAG <sokolov.ag01@gmail.com>
2020-12-01 23:24:24 +0300

    рефакРефакÑПереписал существ имеющиеся компоненты на классы

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,3 +4,26 @@ export const getRandomInteger = (start = 0, end = 1) => {
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`); // 1
+  newElement.innerHTML = template; // 2
+
+  return newElement.firstChild; // 3
+};
